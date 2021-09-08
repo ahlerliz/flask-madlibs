f750d634e5508f9c57bb33d45891884362914d86
Colin Sidberry <colinsidberry@Colins-MacBook-Pro.local>
2021-09-08 16:51:47 -0700

    first code review

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-from flask.templating import render_template_string
 from flask_debugtoolbar import DebugToolbarExtension
 
 from stories import silly_story
